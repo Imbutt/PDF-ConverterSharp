715e4b8acfb51072973be69712bdea4b44683a0f
Inbutt <urtomer@gmail.com>
2021-03-23 22:06:58 +0100

    First Commit

---
+++ PdfConverter/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace PdfConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonAddImage = new System.Windows.Forms.Button();
+            this.panelPreview = new System.Windows.Forms.Panel();
+            this.webBrowserTermplate = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButtonFile = new System.Windows.Forms.ToolStripDropDownButton();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialogFile = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialogFile = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogImage = new System.Windows.Forms.OpenFileDialog();
+            this.buttonImageTemplate = new System.Windows.Forms.Button();
+            this.buttonRemoveImage = new System.Windows.Forms.Button();
+            this.buttonMoveImage = new System.Windows.Forms.Button();
+            this.panelPreview.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonAddImage
+            // 
+            this.buttonAddImage.Location = new System.Drawing.Point(358, 441);
+            this.buttonAddImage.Name = "buttonAddImage";
+            this.buttonAddImage.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddImage.TabIndex = 0;
+            this.buttonAddImage.Text = "Add Image";
+            this.buttonAddImage.UseVisualStyleBackColor = true;
+            this.buttonAddImage.Click += new System.EventHandler(this.buttonAddImage_Click);
+            // 
+            // panelPreview
+            // 
+            this.panelPreview.AutoScroll = true;
+            this.panelPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelPreview.Controls.Add(this.buttonImageTemplate);
+            this.panelPreview.Location = new System.Drawing.Point(358, 28);
+            this.panelPreview.Name = "panelPreview";
+            this.panelPreview.Size = new System.Drawing.Size(273, 407);
+            this.panelPreview.TabIndex = 1;
+            // 
+            // webBrowserTermplate
+            // 
+            this.webBrowserTermplate.Location = new System.Drawing.Point(12, 28);
+            this.webBrowserTermplate.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserTermplate.Name = "webBrowserTermplate";
+            this.webBrowserTermplate.Size = new System.Drawing.Size(330, 433);
+            this.webBrowserTermplate.TabIndex = 4;
+            this.webBrowserTermplate.Visible = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButtonFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(796, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButtonFile
+            // 
+            this.toolStripDropDownButtonFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButtonFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.toolStripDropDownButtonFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButtonFile.Image")));
+            this.toolStripDropDownButtonFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButtonFile.Name = "toolStripDropDownButtonFile";
+            this.toolStripDropDownButtonFile.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButtonFile.Text = "File";
+            this.toolStripDropDownButtonFile.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveFileDialogFile
+            // 
+            this.saveFileDialogFile.RestoreDirectory = true;
+            // 
+            // openFileDialogFile
+            // 
+            this.openFileDialogFile.FileName = "openFileDialogFile";
+            // 
+            // openFileDialogImage
+            // 
+            this.openFileDialogImage.FileName = "openFileDialog1";
+            this.openFileDialogImage.RestoreDirectory = true;
+            // 
+            // buttonImageTemplate
+            // 
+            this.buttonImageTemplate.Location = new System.Drawing.Point(3, 3);
+            this.buttonImageTemplate.Name = "buttonImageTemplate";
+            this.buttonImageTemplate.Size = new System.Drawing.Size(120, 120);
+            this.buttonImageTemplate.TabIndex = 0;
+            this.buttonImageTemplate.Text = "button1";
+            this.buttonImageTemplate.UseVisualStyleBackColor = true;
+            this.buttonImageTemplate.Visible = false;
+            // 
+            // buttonRemoveImage
+            // 
+            this.buttonRemoveImage.Location = new System.Drawing.Point(439, 441);
+            this.buttonRemoveImage.Name = "buttonRemoveImage";
+            this.buttonRemoveImage.Size = new System.Drawing.Size(93, 23);
+            this.buttonRemoveImage.TabIndex = 6;
+            this.buttonRemoveImage.Text = "Remove Image";
+            this.buttonRemoveImage.UseVisualStyleBackColor = true;
+            this.buttonRemoveImage.Click += new System.EventHandler(this.buttonRemoveImage_Click);
+            // 
+            // buttonMoveImage
+            // 
+            this.buttonMoveImage.Location = new System.Drawing.Point(538, 441);
+            this.buttonMoveImage.Name = "buttonMoveImage";
+            this.buttonMoveImage.Size = new System.Drawing.Size(93, 23);
+            this.buttonMoveImage.TabIndex = 7;
+            this.buttonMoveImage.Text = "Move Image";
+            this.buttonMoveImage.UseVisualStyleBackColor = true;
+            this.buttonMoveImage.Click += new System.EventHandler(this.buttonMoveImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 500);
+            this.Controls.Add(this.buttonMoveImage);
+            this.Controls.Add(this.buttonRemoveImage);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowserTermplate);
+            this.Controls.Add(this.panelPreview);
+            this.Controls.Add(this.buttonAddImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelPreview.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAddImage;
+        private System.Windows.Forms.Panel panelPreview;
+        private System.Windows.Forms.WebBrowser webBrowserTermplate;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButtonFile;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImage;
+        private System.Windows.Forms.Button buttonImageTemplate;
+        private System.Windows.Forms.Button buttonRemoveImage;
+        private System.Windows.Forms.Button buttonMoveImage;
+    }
+}
+
---
+++ PdfConverter/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+
+
+namespace PdfConverter
+{
+    public partial class Form1 : Form
+    {
+
+        private static PdfDocument Document { get; set; } = new PdfDocument();
+        public static List<RadioButton> ButtPicList { get; set; } = new List<RadioButton>();
+        public int HighlightedImage { get; set; } = -1;
+
+        public string OutputFileName { get; set; } = String.Empty;
+        public static WebBrowser PDFReader { get; set; }
+        public int TempFileNum { get; set; } = 0;
+        private static List<Button> ControlButtonsList { get; set; } 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ControlButtonsList = new List<Button>()
+            {
+                buttonRemoveImage,
+                buttonMoveImage
+            };
+
+            Document.Info.Title = "Created with PDFSharp";
+
+            foreach (Button button in ControlButtonsList)
+            {
+                button.Enabled = false;
+            }
+            
+        }
+
+        private void buttonAddImage_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialogImage.ShowDialog() == DialogResult.OK)
+            {
+                PdfPage page = Document.AddPage();
+                XGraphics gfx = XGraphics.FromPdfPage(page);    // Pdf Graphics class
+
+                string filePath = openFileDialogImage.FileName;
+
+
+                if (IsImageValid(filePath))
+                {
+                    XImage image = XImage.FromFile(filePath);    // Get image
+
+                    int[] ps = GetAutoSizeParams(image.PixelWidth, image.PixelHeight,
+                        (int)page.Width, (int)page.Height);
+                    gfx.DrawImage(image, 0, ps[1], ps[2], ps[3]);   // Draw image to pdf page
+                    UpdatePdf();
+
+                    
+                    int offset = 5;
+
+                    RadioButton buttPic = new RadioButton();  // Add Image button object
+                    buttPic.AutoSize = false;
+                    buttPic.Appearance = Appearance.Button;
+
+                    // Posizion and size the button
+                    buttPic.Location = new Point(
+                        (buttonImageTemplate.Width + offset) *
+                        (ButtPicList.Count % 2) ,
+
+                        (buttonImageTemplate.Height + offset) * 
+                        Math.Abs(ButtPicList.Count / 2));
+                    buttPic.Size = new Size(buttonImageTemplate.Width, buttonImageTemplate.Height);
+
+                    // Add Image to the button
+                    buttPic.BackgroundImage = Image.FromFile(filePath);
+                    buttPic.BackgroundImageLayout = ImageLayout.Stretch;
+
+                    //Events
+                    buttPic.Click += new EventHandler(Image_Click);
+                    this.panelPreview.Controls.Add(buttPic);
+
+                    ButtPicList.Add(buttPic); // Add to list
+                }
+                else
+                    MessageBox.Show("Image invalid or corrupted","ERROR");
+
+
+
+
+
+            }
+
+
+        }
+
+        public void Image_Click(object sender,EventArgs e)
+        {
+            // Highlight image
+            RadioButton buttPic = sender as RadioButton;
+            HighlightedImage = ButtPicList.FindIndex(x => x == buttPic);
+            foreach (Button button in ControlButtonsList)
+            {
+                button.Enabled = true;
+            }
+        }
+
+        private static int[] GetAutoSizeParams(int iw,int ih, int pw, int ph)
+        {
+            // Resize image
+            int w = Math.Min(iw, pw);
+            int h = ih * w / iw;
+            // Center image
+            int x = (iw - w) / 2;
+            int y = (ph - h) / 2;
+
+            return new int[] { x, y, w, h };
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+                
+        }
+
+        private bool IsImageValid(string filePath)
+        {
+            try
+            {
+                using (var bmp = new Bitmap(filePath))
+                {
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void UpdatePdf()
+        {
+            if (TempFileNum == 0)
+                TempFileNum = 1;
+            else
+                TempFileNum = 0;
+
+            if (PDFReader != null)
+            {
+                this.Controls.Remove(PDFReader);
+                PDFReader.Dispose();
+            }
+
+            PDFReader = new WebBrowser();
+            PDFReader.Size = webBrowserTermplate.Size;
+            PDFReader.Location = webBrowserTermplate.Location;
+            this.Controls.Add(PDFReader);
+            
+
+            string tempPath = Path.GetTempPath() + $"PDFConverterTEMP{TempFileNum}.pdf";
+            Document.Close();
+            Document.Save(tempPath);
+
+            PDFReader.Navigate(tempPath);
+
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialogFile.ShowDialog() == DialogResult.OK)
+            {
+                Document.Save(saveFileDialogFile.FileName);
+            }
+        }
+
+        private void buttonRemoveImage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMoveImage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
